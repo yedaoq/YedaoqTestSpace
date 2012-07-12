8c69dd74c318a4c9d0616c4a625fd2d811544558
yedaoq <yedaoq@gmail.com>
2012-07-12 12:13:55 +0800

    Normal

--- Win32TestSolution/ShellExtTest/ShellExtTest.cpp
+++ Win32TestSolution/ShellExtTest/ShellExtTest.cpp
@@ -43,26 +43,28 @@ STDAPI DllInstall(BOOL bInstall, LPCWSTR pszCmdLine)
     HRESULT hr = E_FAIL;
     static const wchar_t szUserSwitch[] = _T("user");
 
-    if (pszCmdLine != NULL)
-    {
-    	if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
-    	{
-    		AtlSetPerUserRegistration(true);
-    	}
-    }
-
-    if (bInstall)
-    {	
-    	hr = DllRegisterServer();
-    	if (FAILED(hr))
-    	{	
-    		DllUnregisterServer();
-    	}
-    }
-    else
-    {
-    	hr = DllUnregisterServer();
-    }
+//     if (pszCmdLine != NULL)
+//     {
+//     	if (_wcsnicmp(pszCmdLine, szUserSwitch, _countof(szUserSwitch)) == 0)
+//     	{
+//     		AtlSetPerUserRegistration(true);
+//     	}
+//     }
+// 
+//     if (bInstall)
+//     {	
+//     	hr = DllRegisterServer();
+//     	if (FAILED(hr))
+//     	{	
+//     		DllUnregisterServer();
+//     	}
+//     }
+//     else
+//     {
+//     	hr = DllUnregisterServer();
+//     }
+	
+	return S_OK;
 
     return hr;
 }
--- Win32TestSolution/ShellExtTest/SimpleShlExt.cpp
+++ Win32TestSolution/ShellExtTest/SimpleShlExt.cpp
@@ -3,6 +3,7 @@
 #include "stdafx.h"
 #include "SimpleShlExt.h"
 #include "CreateProcessHelper.h"
+#include <shlobj.h>
 
 // CSimpleShlExt
 
@@ -10,8 +11,66 @@ HRESULT CSimpleShlExt::Initialize ( LPCITEMIDLIST pidlFolder, LPDATAOBJECT pData
 {
 	//MessageBox(NULL, TEXT("test"), TEXT("test"), MB_OK);
 	CCreateProcessHelper::CreateProcessDetached(TEXT("E:\\YedaoqTestSpace\\Win32TestSolution\\ResLoadTest\\Debug\\ResLoadTest.exe"), 0);
-	return S_OK;
-}
+	
+	FORMATETC fmt = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL }; 
+	STGMEDIUM stg = { TYMED_HGLOBAL };
+	TCHAR	buf[MAX_PATH];
+	HDROP hDrop; 
+
+	if (!pDataObj)
+	{
+		// 当在文件夹中空白位置点击右键时，pDataObj为空指针
+
+		// 1. 根据pidlFolder查询文件夹路径的方法1
+		SHGetPathFromIDList(pidlFolder, buf);
+
+		// 2. 根据pidlFolder查询文件夹路径的方法2 , available above vista
+		//wchar_t* ret;
+		//SHGetNameFromIDList(pidlFolder, SIGDN_FILESYSPATH, &ret);		
+
+		return S_OK;
+	}
+
+	// 在数据对象内查找 CF_HDROP 型数据. 
+	if ( FAILED( pDataObj->GetData ( &fmt, &stg ))) 
+	{ 
+		// Nope! Return an "invalid argument" error back to Explorer.
+		return E_INVALIDARG; 
+	}
+
+	// 获得指向实际数据的指针
+	hDrop = (HDROP) GlobalLock ( stg.hGlobal );
+
+	// 检查非NULL. 
+	if ( NULL == hDrop ) 
+	{ 
+		return E_INVALIDARG;
+	} 
+
+	// 有效性检查 – 保证最少有一个文件名. 
+	UINT uNumFiles = DragQueryFile ( hDrop, 0xFFFFFFFF, NULL, 0 ); 
+	if ( 0 == uNumFiles )
+	{ 
+		GlobalUnlock ( stg.hGlobal );
+		ReleaseStgMedium ( &stg ); 
+		return E_INVALIDARG; 
+	} 
+	HRESULT hr = S_OK; 
+
+	// 取得第一个文件名并把它保存在类成员m_szFile 中.
+	for (int i = 0; i < uNumFiles; ++i)
+	{
+		if ( 0 == DragQueryFile ( hDrop, i, buf, MAX_PATH ))
+		{ 
+			hr = E_INVALIDARG; 
+			break;
+		} 
+	}
+	
+	GlobalUnlock ( stg.hGlobal ); 
+	ReleaseStgMedium ( &stg ); 
+	return hr; 
+} 
 
 HRESULT CSimpleShlExt::QueryContextMenu ( HMENU hmenu, UINT uMenuIndex, UINT uidFirstCmd, UINT uidLastCmd, UINT uFlags )
 {
