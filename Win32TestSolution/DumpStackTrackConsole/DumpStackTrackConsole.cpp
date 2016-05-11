485e015893a35fe30654cd7b8407d615b898711d
yedaoq <yedaoq@gmail.com>
2016-05-11 15:37:50 +0800

    normal

--- Win32TestSolution/DumpStackTrackConsole/DumpStackTrackConsole.cpp
+++ Win32TestSolution/DumpStackTrackConsole/DumpStackTrackConsole.cpp
@@ -7,6 +7,7 @@
 #include "breakpad/client/windows/handler/exception_handler.h"
 #include "base/process_util.h"
 #include <shlwapi.h>
+#include <strsafe.h>
 
 #pragma comment(lib, "base.lib")
 #pragma comment(lib, "base_static.lib")
@@ -21,6 +22,8 @@ bool MyExecptionCallback(const wchar_t *dump_path, const wchar_t *id, void *cont
 int Error();
 int Error1();
 int Error2();
+
+void PrintLastError(LPCTSTR fun_name);
 
 int dump_stack(unsigned long ebp, void **stack, int size) {
 	int depth = 0;
@@ -52,33 +55,74 @@ int _tmain(int argc, _TCHAR* argv[])
 
 bool MyExecptionCallback( const wchar_t *dump_path, const wchar_t *id, void *context, EXCEPTION_POINTERS *exinfo, MDRawAssertionInfo *assertion, bool succeeded )
 {
-
-
-	void* stack[20] = {0};
-
-	MEMORY_BASIC_INFORMATION MemInfo;
-
-	// VirtualQuery can be used to get the allocation base associated with a
-	// code address, which is the same as the ModuleHandle. This can be used
-	// to get the filename of the module that the crash happened in.
-	if (VirtualQuery((void*)exinfo->ContextRecord->Eip, &MemInfo, sizeof(MemInfo)))
-	{
-		stack[0] = MemInfo.AllocationBase;
-	}
-
-	stack[1] = (void*)Error2;
-	stack[2] = (void*)Error1;
-	stack[3] = (void*)Error;
-
-
-	dump_stack(exinfo->ContextRecord->Ebp, stack + 4, 18);
-
-	for (int i = 0; stack[i];++i)
-	{
-		char buf[20];
-		sprintf(buf, "0x%08X\r\n", stack[i]);
-		OutputDebugStringA(buf);
-	}
+// 	void* stack[30] = {0};
+// 	int	  stack_idx = 0;
+// 
+// 	HANDLE process_handle = GetCurrentProcess();
+// 	DuplicateHandle(process_handle, process_handle, process_handle, &process_handle, NULL, FALSE, DUPLICATE_SAME_ACCESS);
+// 
+// 	STACKFRAME64 stack_frame = {0};
+// 	stack_frame.AddrPC.Mode = AddrModeFlat;
+// 	stack_frame.AddrPC.Offset = exinfo->ContextRecord->Eip;
+// 	stack_frame.AddrStack.Mode = AddrModeFlat;
+// 	stack_frame.AddrStack.Offset = exinfo->ContextRecord->Esp;
+// 	stack_frame.AddrFrame.Mode = AddrModeFlat;
+// 	stack_frame.AddrFrame.Offset = exinfo->ContextRecord->Ebp;
+// 
+// 	CONTEXT  thread_context_1 = {0};
+// 
+// 	stack[stack_idx++] = (void*)stack_frame.AddrPC.Offset;
+// 
+// 	if(!StackWalk64(IMAGE_FILE_MACHINE_I386, process_handle, GetCurrentThread(),  &stack_frame, &thread_context_1, NULL, SymFunctionTableAccess64, SymGetModuleBase64, NULL ))
+// 		PrintLastError(TEXT("StackWalk64"));
+// 
+// 	stack[stack_idx++] = (void*)stack_frame.AddrPC.Offset;
+// 	stack[stack_idx++] = (void*)thread_context_1.Eip;
+// 
+// 	if(!StackWalk64(IMAGE_FILE_MACHINE_I386, process_handle, GetCurrentThread(),  &stack_frame, &thread_context_1, NULL, SymFunctionTableAccess64, SymGetModuleBase64, NULL ))
+// 		PrintLastError(TEXT("StackWalk641"));
+// 
+// 	stack[stack_idx++] = (void*)stack_frame.AddrPC.Offset;
+// 	stack[stack_idx++] = (void*)thread_context_1.Eip;
+// 
+// 	if(!StackWalk64(IMAGE_FILE_MACHINE_I386, process_handle, NULL,  &stack_frame, &thread_context_1, NULL, NULL, NULL, NULL ))
+// 		PrintLastError(TEXT("StackWalk642"));
+// 
+// 	stack[stack_idx++] = (void*)stack_frame.AddrPC.Offset;
+// 	stack[stack_idx++] = (void*)thread_context_1.Eip;
+// 
+// 
+// 
+// 
+// 
+// 
+// 	MEMORY_BASIC_INFORMATION MemInfo;
+// 
+// 	stack[stack_idx++] = (void*)Error;
+// 	stack[stack_idx++] = (void*)Error1;
+// 	stack[stack_idx++] = (void*)Error2;
+// 	
+// 	stack[stack_idx++] = (void*)1;
+// 
+// 	// VirtualQuery can be used to get the allocation base associated with a
+// 	// code address, which is the same as the ModuleHandle. This can be used
+// 	// to get the filename of the module that the crash happened in.
+// 	if (VirtualQuery((void*)exinfo->ContextRecord->Eip, &MemInfo, sizeof(MemInfo)))
+// 	{
+// 		stack[stack_idx++] = MemInfo.AllocationBase;
+// 	}
+// 
+// 	stack[stack_idx++] = (void*)1;
+// 
+// 
+// 	dump_stack(exinfo->ContextRecord->Ebp, stack + stack_idx, 30 - stack_idx);
+// 
+// 	for (int i = 0; stack[i];++i)
+// 	{
+// 		char buf[20];
+// 		sprintf(buf, "0x%08X\r\n", stack[i]);
+// 		OutputDebugStringA(buf);
+// 	}
 
 	return true;
 }
@@ -101,7 +145,6 @@ int Error1()
 
 bool MyFilterCallback( void* context, EXCEPTION_POINTERS* exinfo, MDRawAssertionInfo* assertion )
 {
-	OutputDebugStringA("test");
 	return true;
 }
 
@@ -111,4 +154,29 @@ int Error()
 	int a = GetTickCount();
 	int b = a % 0xFFFFFFFFFF;
 	return 1/(b - a);
+}
+
+void PrintLastError(LPCTSTR fun_name)
+{
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	DWORD dw = GetLastError(); 
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf,
+		0, NULL );
+
+	// Display the error message and exit the process
+
+	TCHAR lpDisplayBuf[1024];
+
+	StringCchPrintf((LPTSTR)lpDisplayBuf, ARRAYSIZE(lpDisplayBuf), TEXT("%s error %d: %s\r\n"), fun_name, dw, lpMsgBuf); 
+	OutputDebugString(lpDisplayBuf);
+
+	LocalFree(lpMsgBuf);
 }
