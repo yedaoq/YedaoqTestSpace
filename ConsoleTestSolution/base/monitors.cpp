391725687477492be40f7b8ef9509b8a93bd44c8
yedaoq <yedaoq@gmail.com>
2017-07-19 15:03:46 +0800

    normal

--- ConsoleTestSolution/base/monitors.cpp
+++ ConsoleTestSolution/base/monitors.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <Windows.h>
+#include <locale.h>
 
 struct DeviceFlagInfo
 {
@@ -22,11 +23,8 @@ DeviceFlagInfo device_flags[] =
 	{DISPLAY_DEVICE_DISCONNECT, TEXT("Disconnect") },
 };
 
-
-int _tmain_monitors(int argc, _TCHAR* argv[])
+void PrinfDisplayDevices()
 {
-	setlocale(LC_ALL,"Chinese-simplified");
-
 	DISPLAY_DEVICE device;
 	device.cb = sizeof(device);
 
@@ -35,9 +33,9 @@ int _tmain_monitors(int argc, _TCHAR* argv[])
 	{
 		if (!EnumDisplayDevices(NULL, idx++, &device, 0))
 			break;
-		
+
 		_tprintf(TEXT("device %d : \r\n\tID   - %s \r\n\tName - %s \r\n\tKey  - %s \r\n\tAdapter  - %s\r\n\tFlag - %08x"), idx, device.DeviceID, device.DeviceName, device.DeviceKey, device.DeviceString, device.StateFlags);
-		
+
 		for (int i = 0; i < ARRAYSIZE(device_flags); ++i)
 		{
 			if (device.StateFlags & device_flags[i].flag)
@@ -65,14 +63,55 @@ int _tmain_monitors(int argc, _TCHAR* argv[])
 			_tprintf(TEXT("(%d, %d*%d, %dHz)"), driver_ver, logic_size.cx, logic_size.cy, refresh_rate);
 			DeleteDC(dc);
 		}
-
 		_tprintf(TEXT("\r\n\r\n"));
-
-
-		EnumMonitors(NULL, 2, )
-			 
 	}
+}
 
+void PrintMonitors()
+{
+	BYTE buf[65535];
+	DWORD bytes_need, objs_returned;
+	if(!EnumMonitors(NULL, 2, buf, ARRAYSIZE(buf), &bytes_need, &objs_returned))
+		printf("fail to print monitor info\r\n\r\n");
+
+	MONITOR_INFO_2* monitor_infos = (MONITOR_INFO_2*)buf;
+	for(int i = 0; i < objs_returned /*bytes_returned / sizeof(*monitor_infos)*/; ++i) // bytes_returned > 0; bytes_returned -= sizeof(*monitor_infos))
+	{
+		_tprintf(TEXT("Monitor %d:\r\n\tName - %s\r\n\tDll  - %s\r\n\tEnv  - %s"), i, monitor_infos->pName, monitor_infos->pDLLName, monitor_infos->pEnvironment);
+		_tprintf(TEXT("\r\n\r\n"));
+	}
+}
+
+int monitor_count = 0;
+
+BOOL CALLBACK MonitorEnumProc( HMONITOR hMonitor,  // handle to display monitor
+							  HDC hdcMonitor,     // handle to monitor DC
+							  LPRECT lprcMonitor, // monitor intersection rectangle
+							  LPARAM dwData)       // data);
+{
+	MONITORINFOEX mi;
+	mi.cbSize = sizeof(mi);
+	++monitor_count;
+	if(!GetMonitorInfo(hMonitor, &mi))
+		printf("fail to get #%d display monitor info\r\n\r\n", monitor_count);
+
+	_tprintf(TEXT("Display Monitor %d:\r\n\tDevice - %s\r\n\tRect   - (%d, %d, %d, %d)\r\n"), monitor_count, mi.szDevice, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right, mi.rcMonitor.bottom);
+	_tprintf(TEXT("\r\n\r\n"));
+	return TRUE;
+}
+
+void PrintDisplayMonitors()
+{
+	monitor_count = 0;
+	EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, NULL);
+}
+
+int _tmain_monitors(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL,"Chinese-simplified");
+	PrinfDisplayDevices();
+	PrintMonitors();
+	PrintDisplayMonitors();
 
 	getchar();
 	return 0;
